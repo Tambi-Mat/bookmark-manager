fb5c3b999848cd95b697d7bcf9b84e5b5b6ba8be
Tambi-Mat <caicaratambi@gmail.com>
2017-07-25 17:48:56 +0100

    created link model; app controller; views; unit and feature tests; linked DataMapperto database. modified gemfiles and spec helper.

--- Gemfile
+++ Gemfile
@@ -2,17 +2,8 @@ source 'https://rubygems.org'
 
 ruby '2.4.0'
 
-gem 'rack'
-gem 'rake'
-gem 'sinatra'
+gem 'capybara'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
-
-group :test do
-  gem 'capybara'
-  gem 'rack-test'
-  gem 'rspec'
-  gem 'rubocop'
-  gem 'simplecov', require: false
-  gem 'simplecov-console', require: false
-end
+gem 'sinatra'
+gem 'rspec'
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,11 +1,12 @@
 feature 'Viewing links' do
   scenario 'I can see existing links on the links page' do
-    Link.create(url: 'https://www.spotify.com/uk/', title: 'Spotify')
+    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
     visit '/links'
+    # save_and_open_page
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-      expect(page).to have_content('Spotify')
+      expect(page).to have_content('Makers Academy')
     end
   end
 end
