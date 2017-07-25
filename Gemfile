c6d0ebcce4ca595570cfd7867189fe9f2c263003
Tambi-Mat <caicaratambi@gmail.com>
2017-07-25 18:33:05 +0100

    installed database_cleaner gem to help clean linked database. gemfiles modified.

--- Gemfile
+++ Gemfile
@@ -7,3 +7,7 @@ gem 'data_mapper'
 gem 'dm-postgres-adapter'
 gem 'sinatra'
 gem 'rspec'
+
+group :test do
+  gem 'database_cleaner'
+end
--- spec/features/viewing_links_spec.rb
+++ spec/features/viewing_links_spec.rb
@@ -1,12 +1,12 @@
 feature 'Viewing links' do
   scenario 'I can see existing links on the links page' do
-    Link.create(url: 'http://www.makersacademy.com', title: 'Makers Academy')
+    Link.create(url: 'https://www.spotify.com/uk/', title: 'Spotify')
     visit '/links'
     # save_and_open_page
     expect(page.status_code).to eq 200
 
     within 'ul#links' do
-      expect(page).to have_content('Makers Academy')
+      expect(page).to have_content('Spotify')
     end
   end
 end
