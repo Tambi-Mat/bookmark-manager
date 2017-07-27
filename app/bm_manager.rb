89ebbdb159051f97380406b24e10f0b0e128eb71
EOSullivanBerlin <esully666@gmail.com>
2017-07-27 13:51:26 +0200

    Multi tags

--- app/bm_manager.rb
+++ app/bm_manager.rb
@@ -15,10 +15,11 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/links' do
-    link = Link.new(url: params[:url], # 1. Craete a link
-                  title: params[:title])
-    tag = Tag.first_or_create(name: params[:tags]) # 2. Create a tag for the link
-    link.tags << tag  # 3. Adding the tag top the link's DataMapper collection
+    link = Link.create(url: params[:url], title: params[:title])
+                  params[:tags].split.each do |tag|
+                    link.tags << Tag.first_or_create(name: tag)
+                  end
+    # 3. Adding the tag top the link's DataMapper collection
     link.save      # 4. Saving link
     redirect to('/links')
   end
