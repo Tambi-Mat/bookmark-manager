7cd144964eec019cd70fdbb557b34c5261ff0d53
EOSullivanBerlin <esully666@gmail.com>
2017-07-27 16:07:36 +0200

    BCrypt installed and user password

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org'
 
 ruby '2.4.0'
 
-
+gem 'bcrypt'
 gem 'database_cleaner'
 gem 'data_mapper'
 gem 'dm-postgres-adapter'
--- app/bm_manager.rb
+++ app/bm_manager.rb
@@ -4,6 +4,8 @@ require 'sinatra/base'
 require_relative 'data_mapper_setup'
 
 class BookmarkManager < Sinatra::Base
+  enable :sessions
+  set :session_secret, 'super secret'
 
   get '/links' do
     @links = Link.all
@@ -29,4 +31,19 @@ class BookmarkManager < Sinatra::Base
     erb :'links/index'
   end
 
+  post '/users' do
+    user = User.create(email: params[:email], password: params[:password])
+    session[:user_id] = user.id
+    redirect to('/links')
+  end
+
+  helpers do
+    def current_user
+      @current_user ||= User.get(session[:user_id])
+    end
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
 end
